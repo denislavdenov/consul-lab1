14d61344be1c00e8f446814a589b8f782fa7137a
Denov <denov@Denovs-MacBook-Pro.local>
2019-01-25 10:30:56 +0200

    adding changes

--- scripts/check_nginx.sh
+++ scripts/check_nginx.sh
@@ -13,7 +13,7 @@ var1=$(hostname)
 cat << EOF > /usr/local/bin/check_wel.sh
 #!/usr/bin/env bash
 
-curl localhost:80 | grep "Welcome to"
+curl 127.0.0.1:80 | grep "Welcome to"
 EOF
 
 chmod +x /usr/local/bin/check_wel.sh
@@ -30,7 +30,7 @@ cat << EOF > /cclient/.consul.d/web.json
         {
             "id": "nginx_http_check",
             "name": "Check nginx1",
-            "http": "http://localhost:80",
+            "http": "http://127.0.0.1:80",
             "tls_skip_verify": false,
             "method": "GET",
             "interval": "10s",
@@ -39,7 +39,7 @@ cat << EOF > /cclient/.consul.d/web.json
         {
             "id": "nginx_tcp_check",
             "name": "TCP on port 80",
-            "tcp": "localhost:80",
+            "tcp": "127.0.0.1:80",
             "interval": "10s",
             "timeout": "1s"
         },
